ce784210478bb4947ac4fe1059d486d030f66ebe
Subhransu <spanda@tavisca.in>
2019-06-26 01:34:17 +0530

    Problem 3 solution

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
@@ -40,8 +41,71 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            int[] calorie = new int[protein.Length];
+            for (int i = 0; i < protein.Length; i++)
+                calorie[i] = protein[i] * 5 + carbs[i] * 5 + fat[i] * 9;
+            int[] bestFood = new int[dietPlans.Length];
+            for(int i=0;i<dietPlans.Length;i++)
+            {
+                List<int> Temp = new List<int>();
+                for (int j = 0; j < protein.Length; j++)
+                    Temp.Add(j);
+                
+                    for(int j=0;j<dietPlans[i].Length;j++)
+                    {
+                        if (dietPlans[i][j] == 'C')
+                            Temp = MaxCompare(carbs, Temp);
+                        else if (dietPlans[i][j] == 'c')
+                            Temp = MinCompare(carbs, Temp);
+                        else if (dietPlans[i][j] == 'P')
+                            Temp = MaxCompare(protein, Temp);
+                        else if (dietPlans[i][j] == 'p')
+                            Temp = MinCompare(protein, Temp);
+                        else if (dietPlans[i][j] == 'F')
+                            Temp = MaxCompare(fat, Temp);
+                        else if (dietPlans[i][j] == 'f')
+                            Temp = MinCompare(fat, Temp);
+                        else if (dietPlans[i][j] == 'T')
+                            Temp = MaxCompare(calorie, Temp);
+                        else if (dietPlans[i][j] == 't')
+                            Temp = MinCompare(calorie, Temp);
+                        if (Temp.Count == 1)
+                        {
+                            bestFood[i] = Temp[0];
+                            break;
+                        }
+
+                    }
+                if (Temp.Count > 1)
+                    bestFood[i] = Temp[0];
+
+            }
+            return bestFood;
+        }
+
+        public static List<int> MaxCompare(int[] arr, List<int> Temp)
+        {
+            List<int> Temp1 = new List<int>();
+            int max = arr[Temp[0]];
+            for (int i = 1; i < Temp.Count; i++)
+                if (arr[Temp[i]] > max)
+                    max = arr[Temp[i]];
+            for (int i = 0; i < Temp.Count; i++)
+                if (arr[Temp[i]] == max)
+                    Temp1.Add(Temp[i]);
+            return Temp1;
+        }
+        public static List<int> MinCompare(int[] arr, List<int> Temp)
+        {
+            List<int> Temp1 = new List<int>();
+            int min = arr[Temp[0]];
+            for (int i = 1; i < Temp.Count; i++)
+                if (arr[Temp[i]] < min)
+                    min = arr[Temp[i]];
+            for (int i = 0; i < Temp.Count; i++)
+                if (arr[Temp[i]] == min)
+                    Temp1.Add(Temp[i]);
+            return Temp1;
         }
     }
 }
